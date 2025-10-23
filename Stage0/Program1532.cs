590293ddd7e269c91e491d809b2fca0f0b702c5e
yoelyek <yoelmoshey@gmail.com>
2025-10-23 12:52:54 +0300

    Inital code + Welcome functions

---
+++ Stage0/Program1532.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1532();
+            Welcome0504();
+            Console.ReadKey();
+        }
+
+        private static void Welcome1532()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+        static partial void Welcome0504();
+    }
+}
