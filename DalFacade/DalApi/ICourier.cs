4ac965172b8974129b7a9e490130d21a5894ef67
DFranko77 <dfrank@g.jct.ac.il>
2025-11-04 18:07:03 +0200

    DalApi Created

---
+++ DalFacade/DalApi/ICourier.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DalApi;
+using DO;
+
+public interface ICourier
+{
+    void Create(Order item); //Creates new entity object in DAL
+    Order? Read(int id); //Reads entity object by its ID 
+    List<Order> ReadAll(); //stage 1 only, Reads all entity objects
+    void Update(Order item); //Updates entity object
+    void Delete(int id); //Deletes an object by its Id
+    void DeleteAll(); //Delete all entity objects
+}
