8df3fa77d057373da7f977d8274e9d6a36805b90
DFranko77 <dfrank@g.jct.ac.il>
2025-11-04 20:13:23 +0200

    IConfig and ConfigImplementation

--- DalFacade/DalApi/ICourier.cs
+++ DalFacade/DalApi/ICourier.cs
@@ -4,10 +4,10 @@ using DO;
 
 public interface ICourier
 {
-    void Create(Order item); //Creates new entity object in DAL
-    Order? Read(int id); //Reads entity object by its ID 
-    List<Order> ReadAll(); //stage 1 only, Reads all entity objects
-    void Update(Order item); //Updates entity object
+    void Create(Courier item); //Creates new entity object in DAL
+    Courier? Read(int id); //Reads entity object by its ID 
+    List<Courier> ReadAll(); //stage 1 only, Reads all entity objects
+    void Update(Courier item); //Updates entity object
     void Delete(int id); //Deletes an object by its Id
     void DeleteAll(); //Delete all entity objects
 }
