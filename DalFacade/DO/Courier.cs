452429c56cdae920976b2a5d5fe83c737cb187ab
DFranko77 <dfrank@g.jct.ac.il>
2025-11-04 17:17:05 +0200

    Enums, Order, Courier, Delivery Updated

--- DalFacade/DO/Courier.cs
+++ DalFacade/DO/Courier.cs
@@ -1,19 +1,33 @@
-﻿
-
+﻿// Module Courier.cs
 namespace DO;
-
+/// <summary>
+/// Entity that represents a courier in the delivery system.
+/// </summary>
+/// <param name="Id">Personal unique ID of the courier</param>
+/// <param name="FullName">Full name of the courier</param>
+/// <param name="PhoneNum">Contact phone number of the courier</param>
+/// <param name="Email">Contact email address of the courier</param>
+/// <param name="Password">Password for courier's account</param>
+/// <param name="IsActive">Indicates if the courier is currently active</param>
+/// <param name="MaxDist">Maximum distance the courier is willing to travel</param>
+/// <param name="PreferredShippingMethod">Preferred method of shipping for the courier</param>
+/// <param name="DayStarted">Date when the courier started working</param>
 public record Courier
-   (
+(
     int Id,
-    string FullName,
-    string PhoneNum,
-    string Email,
-    string Password,
-    bool IsActive = false,
+    string FullName, 
+    string PhoneNum, 
+    string Email, 
+    string Password, 
+    bool IsActive = false,  
     double? MaxDist = null,
-    //enum DELTYPE { CAR , MOTORBIKE, BIKE, ONFOOT},
-    DateTime? DayStarted = null   /// check
-    )
+    ShippingMethod? PreferredShippingMethod = null, 
+    DateTime? DayStarted = null 
+)
+
+/// <summary>
+/// Default constructor for Courier with default values.
+/// <summary>
 {
     public Courier() : this(0, "", "", "", ""){ }
 }
--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,7 +1,35 @@
 ﻿namespace DO;
 
-internal class Class1
+/// <summary>
+/// Methods of shipping available
+/// </summary>
+public enum ShippingMethod 
 {
+    Car,
+    Motorcycle,
+    Bike,
+    OnFoot
 }
-// DeliveryType
-// DeliveryEndType
+
+/// <summary>
+/// Represents the types of food available in the system.
+/// </summary>
+public enum  OrderType
+{
+    Pizza,
+    Hamburger,
+    Fries,
+    IceCream
+}
+
+/// <summary>
+/// Types of delivery status
+/// </summary>
+public enum CompletionType
+{
+    Pending,
+    EnRoute,
+    Delivered,
+    Cancelled,
+    Failed
+}
