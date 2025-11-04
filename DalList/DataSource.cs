6c77ec75c9c9298be9c16ff84d083e2d5ec1fbd6
yoelyek <yoelmoshey@gmail.com>
2025-11-04 18:08:22 +0200

    created the list class

---
+++ DalList/DataSource.cs
@@ -0,0 +1,10 @@
+﻿namespace Dal;
+
+internal static class DataSource
+{
+    internal static List<DO.Courier> Couriers { get; } = new();
+    internal static List<DO.Delivery> Deliveries { get; } = new();
+    internal static List<DO.Order> Orders { get; } = new();
+
+
+}
