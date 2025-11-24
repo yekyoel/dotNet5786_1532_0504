a217bcec32fcf76f4f4e111b6536bc3dc86d5eb5
yoelyek <yoelmoshey@gmail.com>
2025-11-24 21:23:54 +0200

    stage 4 - 6

---
+++ BL/BO/Courier.cs
@@ -0,0 +1,28 @@
+﻿namespace BO;
+
+public class Courier
+{
+    public int Id { get; init; }
+    public string FullName { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set;}
+
+    public bool IsActive { get; set;}
+
+    public double? MaxDist { get; set;}
+
+    // public OrderType orderType { get; set;}
+
+    public DateTime EmploymentStartDate { get; init; } 
+
+    public int TotalDelSuppliedOnTime { get; init; }
+
+    public int TotalDelSuppliedLate { get; init; }
+
+    //BO.OrderInProgress huhuh // nullable
+
+}
