37ef732014f77ba8a8e4f7baae249bcf88edb920
DFranko77 <dfrank@g.jct.ac.il>
2025-11-25 18:04:50 +0200

    BO.Order and BO.OrderInList Updated

---
+++ BL/BO/Order.cs
@@ -0,0 +1,23 @@
+﻿namespace BO;
+
+public class Order
+{
+    public int Id { get; init; }
+    OrderType orderType { get; set; }
+    public string Description { get; set; }
+    public string OrderAddress { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double AerialDistance { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerPhone { get; set; }
+    public double Weight { get; set; }
+    public DateTime OrderPlacedTime { get; init; }
+    public DateTime? ExpectedDeliveryTime { get; init; }
+    public DateTime MaxDeliveredTime { get; init; }
+    public OrderStatus OrderStatus { get; init; }
+    public ScheduleStatus ScheduleStatus { get; init; }
+    public TimeSpan TotalTimeLeft { get; init; }
+    //public list<DeliveryPerOrderInList>? DeliveriesList { get; init; }
+
+}
