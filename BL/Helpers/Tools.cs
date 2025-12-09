4eacbcded97cfcad91e821b1da61307cb9e90986
yoel <yyekutie@g.jct.ac.il>
2025-12-09 20:37:49 +0200

    dddd

--- BL/Helpers/Tools.cs
+++ BL/Helpers/Tools.cs
@@ -14,5 +14,54 @@ internal static class Tools
         return obj.GetType().GetProperty(propertyName)?
                   .GetValue(obj, null);
     }
+
+    internal static BO.OrderType? SwitchOrderTypeTOBO(DO.Order order)
+    {
+        return order.Food switch
+        {
+            DO.OrderType.Pizza => BO.OrderType.Pizza,
+            DO.OrderType.Hamburger => BO.OrderType.Hamburger,
+            DO.OrderType.Fries => BO.OrderType.Fries,
+            DO.OrderType.IceCream => BO.OrderType.IceCream,
+            _ => null
+        };
+    }
+
+    internal static DO.OrderType? SwitchOrderTypeTODO(BO.Order order)
+    {
+        return order.OrderTyype switch
+        {
+            BO.OrderType.Pizza => DO.OrderType.Pizza,
+            BO.OrderType.Hamburger => DO.OrderType.Hamburger,
+            BO.OrderType.Fries => DO.OrderType.Fries,
+            BO.OrderType.IceCream => DO.OrderType.IceCream,
+            _ => null
+        };
+    }
+
+    internal static BO.OrderStatus? FindOrderStatusType(BO.OrderStatus? status)
+    {
+        return status switch
+        {
+            BO.OrderStatus.Open => BO.OrderStatus.Open,
+            BO.OrderStatus.InProgress => BO.OrderStatus.InProgress,
+            BO.OrderStatus.Completed => BO.OrderStatus.Completed,
+            BO.OrderStatus.Rejected => BO.OrderStatus.Rejected,
+            BO.OrderStatus.Cancelled => BO.OrderStatus.Cancelled,
+            _ => null
+        };
+    }
+
+    internal static BO.ScheduleStatus? SwitchScheduleStatusTOBO(BO.ScheduleStatus? status)
+    {
+        return status switch
+        {
+            BO.ScheduleStatus.OnTime => BO.ScheduleStatus.OnTime,
+            BO.ScheduleStatus.InRisk => BO.ScheduleStatus.InRisk,
+            BO.ScheduleStatus.Late => BO.ScheduleStatus.Late,
+            _ => null
+        };
+    }
+}
 }
 
