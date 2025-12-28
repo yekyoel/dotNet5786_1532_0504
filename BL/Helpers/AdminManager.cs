28194e14c147fc435bce344b2257b4c3b8bf1b31
yoel <yyekutie@g.jct.ac.il>
2025-12-28 19:56:56 +0200

    fixed adamin id

--- BL/Helpers/AdminManager.cs
+++ BL/Helpers/AdminManager.cs
@@ -158,7 +158,8 @@ internal static class AdminManager //stage 4
         {
             s_dal.ResetDB(); //stage 4
             AdminManager.UpdateClock(AdminManager.Now); //stage 5 - needed since we want the label on Pl to be updated
-            AdminManager.SetConfig(AdminManager.GetConfig()); //stage 5 - needed to update PL 
+            //AdminManager.SetConfig(AdminManager.GetConfig()); //stage 5 - needed to update PL 
+            ConfigUpdatedObservers?.Invoke();
         }
     }
 
@@ -168,7 +169,8 @@ internal static class AdminManager //stage 4
         {
             DalTest.Initialization.Do(); //stage 4
             AdminManager.UpdateClock(AdminManager.Now);  //stage 5 - needed since we want the label on Pl to be updated           
-            AdminManager.SetConfig(AdminManager.GetConfig()); //stage 5 - needed for update the PL
+            //AdminManager.SetConfig(AdminManager.GetConfig()); //stage 5 - needed for update the PL
+            ConfigUpdatedObservers?.Invoke(); 
         }
     }
 
