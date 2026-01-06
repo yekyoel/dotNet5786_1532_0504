a29ba34cfe4165c64938a93bdda679483a8b58f8
yoel <yyekutie@g.jct.ac.il>
2026-01-06 21:51:10 +0200

    finshed the admin screens

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -1,6 +1,7 @@
 ﻿using DO;
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 
@@ -8,15 +9,9 @@ namespace PL;
 
 public class BoolToVisibilityConverter : IValueConverter
 {
-    // parameter = "Invert" to invert logic
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         bool flag = value is bool b && b;
-        bool invert = string.Equals(parameter as string, "Invert", StringComparison.OrdinalIgnoreCase);
-
-        if (invert)
-            flag = !flag;
-
         return flag ? Visibility.Visible : Visibility.Collapsed;
     }
 
@@ -32,7 +27,8 @@ public class AddUpdateTextConverter : IValueConverter
         return isAdd ? "Add" : "Update";
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
 }
 
 public class CanCancelOrderConverter : IValueConverter
@@ -46,24 +42,51 @@ public class CanCancelOrderConverter : IValueConverter
 
         try
         {
-            // Same logic as GetCompletionTypeForOrder + CanCancel
-            var userId = s_bl.Admin.GetConfig().AdminId;
+            int userId = s_bl.Admin.GetConfig().AdminId;
             var order = s_bl.Order.GetOrderDetails(userId, orderId);
 
-            var type = order.DeliveriesList?
+            var lastDelivery = order.DeliveriesList?
                 .OrderByDescending(d => d.DeliveryId)
-                .FirstOrDefault()
-                ?.CompType;
+                .FirstOrDefault();
 
-            // Only allow cancel if not already Cancelled or Delivered
-            return type != BO.CompletionType.Refused
-                   && type != BO.CompletionType.Delivered
-                   && type != BO.CompletionType.Failed
-                   && type != BO.CompletionType.Cancelled;
+            BO.CompletionType? type = lastDelivery?.CompType;
+
+            if (type is null)
+                return true;
+
+            return type != BO.CompletionType.Cancelled
+                   && type != BO.CompletionType.Delivered;
         }
         catch
         {
-            // On error, be safe and disable cancel
+            return true;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
+
+public class CanDeleteCourierConverter : IValueConverter
+{
+    private static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not int courierId || courierId <= 0)
+            return false;
+
+        try
+        {
+            int userId = s_bl.Admin.GetConfig().AdminId;
+            // If courier has any closed deliveries, do not allow delete
+            var deliveries = s_bl.Order.GetCompletedCourierDeliveries(userId, courierId, null, null);
+            bool hasAny = deliveries != null && deliveries.Any();
+            return !hasAny;
+        }
+        catch
+        {
+            // On error, be conservative and disallow delete
             return false;
         }
     }
